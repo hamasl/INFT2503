91bc1e25bd84f480e1782ecf663ba927f39580ff
Martin Hagen <martin@partins.no>
2021-10-23 19:07:48 +0200

    Minor fixes for task 9

--- Task9/main.cpp
+++ Task9/main.cpp
@@ -20,22 +20,22 @@ int main() {
     //Task a
     cout << endl << "Task a:" << endl;
     auto it = find_if(v1.begin(), v1.end(), [](int e){ return e > 15; });
-    cout << "First element larger than 15 is " << *it << " at index " << (it-v1.begin()) << "." << endl;
+    if(it != v1.end())cout << "First element larger than 15 is " << *it << " at index " << (it-v1.begin()) << "." << endl;
+    else cout << "Could not find any element larger than 15" << endl;
 
     //Task b
     cout << endl << "Task b:" << endl;
     //Equality in this context allows a difference of 2 per element
     auto equals = [](int e1, int e2){return abs(e1-e2) <= 2;};
+    //extra anonymous function to display bool return value from equal more readably
     auto bool_to_str = [](bool val){return val ? "true" : "false";};
-    cout << "[v1.begin(), v1.begin() + 5> equal to v2: " << bool_to_str(equal(v1.begin(), v1.begin()+5, v2.begin(), equals)) << endl;
-    cout << "[v1.begin(), v1.begin() + 4> equal to v2: " << bool_to_str(equal(v1.begin(), v1.begin()+4, v2.begin(), equals)) << endl;
+    cout << "[v1.begin(), v1.begin() + 5> approximately equal to v2: " << bool_to_str(equal(v1.begin(), v1.begin()+5, v2.begin(), equals)) << endl;
+    cout << "[v1.begin(), v1.begin() + 4> approximately equal to v2: " << bool_to_str(equal(v1.begin(), v1.begin()+4, v2.begin(), equals)) << endl;
 
 
     //Task c
     cout << endl << "Task c:" << endl;
-    vector<int> res;
-    res.resize(v1.size());
-    replace_copy_if(v1.begin(), v1.end(), res.begin(), [](int e){return (e%2) == 1;}, 100);
-    cout << "res vector is: " << res << endl;
+    replace_copy_if(v1.begin(), v1.end(), v1.begin(), [](int e){return (e%2) == 1;}, 100);
+    cout << "v1 is now: " << v1 << endl;
     return 0;
 }
